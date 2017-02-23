50b2f249adce52f486b05133bc5695574a2a015a
iJayGuoxy <guoxy_24@sina.com>
2017-02-23 10:01:11 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 税云-研发部-iOS on 2017/2/23.
+//  Copyright © 2017年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 税云-研发部-iOS on 2017/2/23.
+//  Copyright © 2017年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
